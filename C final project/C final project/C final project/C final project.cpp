5808b619ac9c07cb55c19348f49fd416cbb579ec
Kevin5465 <kevin050296@gmail.com>
2024-12-28 13:33:35 +0800

    draw-lots-system

--- C final project/C final project/C final project/C final project.cpp
+++ C final project/C final project/C final project/C final project.cpp
@@ -2,6 +2,30 @@
 #include <stdlib.h>
 
 
-int main() {
-   
+void number_system();
+void input_system();
+
+int main(void) {
+	int s;
+	printf("請選擇數字系統或文字系統:");
+	scanf("%d", &s);
+	switch (s)
+	{
+	case 1:
+		number_system();
+		break;
+	case 2:
+		input_system();
+		break;
+	};
+	system("pause");
+	return 0;
+}
+
+void number_system() {
+	printf("1");
+}
+
+void input_system() {
+	printf("2");
 }
